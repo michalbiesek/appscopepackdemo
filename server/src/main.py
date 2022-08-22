46bf268d0a391225477946850b83dcc7b6bfd61e
michalbiesek <mbiesek@cribl.io>
2022-08-22 12:31:37 +0200

    Add Server build

---
+++ server/src/main.py
@@ -0,0 +1,24 @@
+import argparse
+from flask import Flask
+
+app = Flask(__name__)
+ARG_PORT = 'port'
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+
+def main() -> None:
+    """
+    Main function
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(f'--{ARG_PORT}', help='port', default='1234')
+    cli_cfg = vars(parser.parse_args())
+    port = cli_cfg[ARG_PORT]
+
+    app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    main()
